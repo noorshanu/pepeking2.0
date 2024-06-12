a0309b8d38ae4b1cf6b4fa91b2e78de0bfe487c7
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-06-12 20:29:34 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -58,7 +58,7 @@ function Hero() {
               </a>
             </div>
             <button className="py-2 px-4 mt-2 rounded-full bg-[#000] border-2 border-[#8ff852] text-white font-bold transition-all duration-200 hover:bg-transparent hover:text-[#8ff852] text-xs sm:text-sm relative">
-              CA: soon....
+              CA: 81wKRihbkT8cJgGVMCZxnfqpuY6HY2bzMDpLGEox5kK
             </button>
           </div>
         </div>
